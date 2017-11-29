42d534b1df6a8fa4207800a9a0de8c3b41f045b2
Vinícius Pontin <vpontin93@gmail.com>
2017-11-29 20:28:00 -0200

    Criadas primeiras classes e atualizado config-sample
    Ainda não sei se funciona, somente pra começar a estruturar!

--- bot.js
+++ bot.js
@@ -1,19 +1,49 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const crono = new Discord.Client();
+const marle = new Discord.Client();
+const lucca = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+// config.crono/marle/lucca.token contains the given bot's token
+// config.crono/marle/lucca.prefix contains the message prefix.
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+crono.on('ready', () => {
+  console.log(`Crono logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+crono.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.login('token');
+crono.login(config.crono.token);
+
+// --
+
+marle.on('ready', () => {
+  console.log(`Marle logged in as ${client.user.tag}!`);
+});
+
+marle.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+marle.login(config.marle.token);
+
+// --
+
+lucca.on('ready', () => {
+  console.log(`Marle logged in as ${client.user.tag}!`);
+});
+
+lucca.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+lucca.login(config.lucca.token);
