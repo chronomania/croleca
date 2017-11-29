3a052bb342c994d668bf56aa0cb2d4cf4fd81483
Vinícius Pontin <vpontin93@gmail.com>
2017-11-29 20:58:15 -0200

    Empaquei
    Como mandar mensagens de um bot (objeto) dentro de outro??

--- bot.js
+++ bot.js
@@ -8,22 +8,13 @@ const config = require("./config.json");
 // config.crono/marle/lucca.token contains the given bot's token
 // config.crono/marle/lucca.prefix contains the message prefix.
 
-crono.on('ready', () => {
-  console.log(`Crono logged in as ${client.user.tag}!`);
-});
-
-crono.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-crono.login(config.crono.token);
-
 // --
 
 marle.on('ready', () => {
-  console.log(`Marle logged in as ${client.user.tag}!`);
+  // console.log(`Marle logged in as ${marle.user.tag}!`);
+  console.log(`Marle fugiu do castelo, está tretando com ${marle.users.size} usuários, em ${marle.channels.size} canais de ${marle.guilds.size} servidores\\guilds.`);
+  // Alterar Now Playing...
+  marle.user.setGame('saving the World!');
 });
 
 marle.on('message', msg => {
@@ -37,7 +28,10 @@ marle.login(config.marle.token);
 // --
 
 lucca.on('ready', () => {
-  console.log(`Marle logged in as ${client.user.tag}!`);
+  // console.log(`Marle logged in as ${lucca.user.tag}!`);
+  console.log(`Lucca foi à feira do milênio, está tretando com ${lucca.users.size} usuários, em ${lucca.channels.size} canais de ${lucca.guilds.size} servidores\\guilds.`);
+  // Alterar Now Playing...
+  lucca.user.setGame('saving the World!');
 });
 
 lucca.on('message', msg => {
@@ -47,3 +41,22 @@ lucca.on('message', msg => {
 });
 
 lucca.login(config.lucca.token);
+
+// --
+
+crono.on('ready', () => {
+  // console.log(`Crono logged in as ${crono.user.tag}!`);
+  console.log(`Crono acordou, está tretando com ${crono.users.size} usuários, em ${crono.channels.size} canais de ${crono.guilds.size} servidores\\guilds.`);
+  // Alterar Now Playing...
+  crono.user.setGame('saving the World!');
+});
+
+crono.on('message', msg => {
+  if (msg.content === 'hei Crono') {
+    //msg.reply('Pong!');
+	marle.sendMessage(msg.channel, "Ele não responde...");
+	lucca.sendMessage(msg.channel, "Só nós!");
+  }
+});
+
+crono.login(config.crono.token);
